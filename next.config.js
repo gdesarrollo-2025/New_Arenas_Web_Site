86a64f4cf41f6eb37aa972ab6561b78f1de98f98
Carlos Balza <desarrollo@arenassa.com>
2026-02-26 15:48:01 -0500

    fixing on domains for the images from domus

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   images: {
-    domains: [], // Este parámetro permite utilizar imágenes sin optimizar (útil para desarrollo)
+    domains: ["https://s3-us-west-2.amazonaws.com/pictures.domus.la/inmobiliaria_59/"] // Este parámetro permite utilizar imágenes sin optimizar (útil para desarrollo)
   },
   webpack: (config, { isServer, dev }) => {
     // Cuando estamos en modo desarrollo, habilitamos hot reloading para archivos de configuración
